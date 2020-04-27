ed9781774dbc32a34d19e2d9d807911f079d0462
artnotek <allanhermet@hotmail.com>
2020-04-27 14:50:11 +0200

    exo 3

--- exos/exo1.py
+++ exos/exo1.py
@@ -42,13 +42,33 @@ compte3 = compteBancaire('dupont3', 1000)
 ###########################
 
 class point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, *z):
         self.x = x
         self.y = y
+        self.z = z
 
     def toString(self, num):
-        print('P = ',self.x, self.y)
+        print('P = ',self.x, self.y, self.z)
 
 
-p1 = point(2,3)
+p1 = point(2,3.1)
 p1.toString(3)
+
+p2 = point(5, 7, 23)
+# p2.toString(3)
+
+###########################
+################ Exercice 3
+###########################
+
+class DateNaissance:
+    def __init__(self, jour, mois, annee):
+        self.jour = jour
+        self.mois = mois
+        self.annee = annee
+
+    def toString(self):
+        print('la date est: '+ str(self.jour)+'/'+str(self.mois)+'/'+str(self.annee))
+
+date1 = DateNaissance(27, 4, 2020)
+date1.toString()
